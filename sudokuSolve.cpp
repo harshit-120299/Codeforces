8a27b07ca4a8c4f09c43eeac164dc3e7c2d9a0e5
Harshit Jain <harshit120299@gmail.com>
2020-10-17 15:18:31 +0530

    solved questions

--- sudokuSolve.cpp
+++ sudokuSolve.cpp
@@ -1,114 +1,72 @@
-#include <bits/stdc++.h>
+//https://practice.geeksforgeeks.org/problems/solve-the-sudoku/0
+//backtracking
+#include<bits/stdc++.h>
 using namespace std;
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define n 9
+void printSudokuBoard(int sodoku[9][9]) {
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) cout<<sodoku[i][j]<<" ";
+    }
+}
+
+bool isFoundInRow(int sudoku[9][9],int r, int val){
+    for (int i = 0; i < 9; ++i) {
+        if(sudoku[r][i]==val) return true;
+    }
+    return false;
+}
+
+bool isFoundInCol(int sudoku[9][9],int c, int val){
+    for (int i = 0; i < 9; ++i) {
+        if(sudoku[i][c]==val)
+            return true;
+    }
+    return false;
+}
 
 bool isFoundInBox(int sudoku[9][9],int r, int c, int val){
-	int startRow = r - r%3 ; 
+
+    int startRow = r - r%3 ; 
     int startCol = c - c%3 ;
     for (int i = startRow; i < startRow+3; ++i){
         for (int j = startCol; j < startCol+3; ++j){
-            if(sudoku[i][j]==val)
-                return true;
+            if(sudoku[i][j]==val) return true;
         }
     }
     return false;
 }
 
-bool isFoundInRow(int sudoku[9][9],int r, int val){
-	for (int i = 0; i < 9; ++i)	{
-		if(sudoku[r][i]==val)
-			return true;
-	}
-	return false;
-}
-
-bool isFoundInCol(int sudoku[9][9],int c, int val){
-	for (int i = 0; i < 9; ++i)	{
-		if(sudoku[i][c]==val)
-			return true;
-	}
-	return false;
-}
-
-void printSudoku(int sodoku[9][9]) {
-	for (int i = 0; i < 9; ++i) {
-
-		for (int j = 0; j < 9; ++j) {
-			cout<<setw(2)<<sodoku[i][j];
-
-			if((j+1)%3==0) 
-				cout<<" ";
-		}
-		if((i+1)%3==0) cout<<"\n"; 
-
-	    cout<<endl;
-	}
-}
-
-void c_p_c() {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-}
-
-
-bool solveSudoku( int sudoku[9][9], int r, int c ) {
-	if(r>8) return true;
-
-	if(sudoku[r][c]==0) {
-		for ( int val = 1; val <= 9; ++val ) {
-			if (!isFoundInRow(sudoku,r,val) && 
-				!isFoundInCol(sudoku,c,val) && 
-				!isFoundInBox(sudoku,r,c,val) 
-				) {
-					sudoku[r][c]=val;
-					if (c<8) {
-						if (solveSudoku(sudoku,r,c+1)) return true;
-						sudoku[r][c]=0;
-					}
-					else {
-						c=0;
-						if (solveSudoku(sudoku,r+1,c)) return true;
-						sudoku[r][c]=0;
-					}
-			}
-		}
-		return false;
-	}
-	else  {
-		if (c<8) {
-			if (solveSudoku(sudoku,r,c+1)) return true;
-			sudoku[r][c]=0;
-		}
-		else {
-			c=0;
-			if (solveSudoku(sudoku,r+1,c))  return true;
-			sudoku[r][c]=0;
-		}
-		return false;
-		
-	}
-	return false;
+bool sudokusolver(int sudokuBoard[9][9],int i,int j){
+    if(i==n) return true;
+    if(j==n) return sudokusolver(sudokuBoard,i+1,0);
+    if(sudokuBoard[i][j]!=0) return sudokusolver(sudokuBoard,i,j+1);
+    for(int val=0;val<=n;val++) {
+        if( !isFoundInBox(sudokuBoard,i,j,val)
+        && !isFoundInRow(sudokuBoard,i,val)
+        && !isFoundInCol(sudokuBoard,j,val) ) {
+            sudokuBoard[i][j]=val;
+            if(sudokusolver(sudokuBoard,i,j+1)) return true;
+        }
+    }
+    sudokuBoard[i][j]=0;
+    return false;
 }
 
 
 int main() {
-    IOS; c_p_c();
-
-	int sudoku[9][9]={0,2,0, 0,0,4, 3,0,0,
-					  9,0,0, 0,2,0, 0,0,8,
-					  0,0,0, 6,0,9, 0,5,0,
-
-					  0,0,0, 0,0,0, 0,0,1,
-					  0,7,2, 5,0,3, 6,8,0,
-					  6,0,0, 0,0,0, 0,0,0,
-
-					  0,8,0, 2,0,5, 0,0,0,
-					  1,0,0, 0,9,0, 0,0,3,
-					  0,0,9, 8,0,0, 0,6,0};
-
-    solveSudoku(sudoku,0,0);
-	printSudoku(sudoku);
-	return 0;
+    int t;
+    cin>>t;
+    while(t--){
+        int sudokuBoard[n][n];
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                cin>>sudokuBoard[i][j];
+            }
+        }
+        sudokusolver(sudokuBoard,0,0);
+        printSudokuBoard(sudokuBoard);
+        cout<<endl;
+    }
+    
+    return 0;
 }
